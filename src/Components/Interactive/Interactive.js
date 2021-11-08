faa1a97e4a4a5a70eb7496b3a0f23ef9bae6976c
Josef Waller <josef@siriusapplications.com>
2021-11-08 15:29:54 -0800

    Add wrapper around C

--- src/Components/Interactive/Interactive.js
+++ src/Components/Interactive/Interactive.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react'
 import set from 'lodash/set'
 import clone from 'lodash/clone'
+import reject from 'lodash/reject'
+import flatten from 'lodash/flatten'
 import blockMap from './blockMap'
+
+import './Interactive.scss'
 
 export default function Interactive(props) {
   const [blocks, setBlocks] = useState([])
@@ -10,22 +14,33 @@ export default function Interactive(props) {
   const setBlockVariable = (index, varName, varVal) => {
     let blocksClone = clone(blocks)
     set(blocksClone, `[${index}].variableNames[${varName}]`, varVal)
+    console.log(blocksClone)
     setBlocks(blocksClone)
   }
-  console.log(blocks)
+  const allVars = blocks.reduce((acc, b) => [...acc, ...Object.values(b.variableNames)], []).filter(isNaN)
+  console.log(allVars)
+  const getC = blocksLines => `#include <iostream>\nint main() {\n  ${allVars.map(v => `int ${v} = 0;`).join('\n  ')}\n  ${blocksLines.join('\n  ')}\n}`
+  const getPython = blocksLines => blocksLines.join('\n')
+  const getMips = blocksLines => blocksLines.join('\n')
   return (
+  <>
     <div>
-      {blocks.map((b, i) => <div>{blockMap.find(bM => bM.id == b.id).render(
-      b.variableNames,
-      (varName, varValue) => setBlockVariable(i, varName, varValue))}</div>)}
-      {blocks.map(b => <div>{blockMap.find(bM => bM.id == b.id).python(b.variableNames)}</div>)}
-      {blocks.map(b => <div>{blockMap.find(bM => bM.id == b.id).c(b.variableNames)}</div>)}
-      <textarea value={blocks.map(b => blockMap.find(bM => bM.id == b.id).mips(b.variableNames)).join('\n')} />
       {blockMap.map(b => <button onClick={() => setBlocks([...blocks, {
         id: b.id,
-        variableNames: b.initialVars
+        variableNames: clone(b.initialVars)
       }
       ])}>{b.text}</button>)}
     </div>
+    <div>
+      {blocks.map((b, i) => <div>{blockMap.find(bM => bM.id == b.id).render(
+      b.variableNames,
+      (varName, varValue) => console.log(i) || setBlockVariable(i, varName, varValue))}</div>)}
+    </div>
+    <div className='code-container'>
+      <textarea className='code' value={getPython(flatten(blocks.map(b => blockMap.find(bM => bM.id == b.id).python(b.variableNames))))} />
+      <textarea className='code' value={getC(flatten(blocks.map(b => blockMap.find(bM => bM.id == b.id).c(b.variableNames))))} />
+      <textarea className='code' value={getMips(flatten(blocks.map(b => blockMap.find(bM => bM.id == b.id).mips(b.variableNames))))} />
+    </div>
+  </>
   )
 }
--- src/Components/Interactive/blockMap.js
+++ src/Components/Interactive/blockMap.js
@@ -13,9 +13,16 @@ export default [
       Add {getVariableInput(vars, setVars, 'v2')}
       to {getVariableInput(vars, setVars, 'v1')}
     </>,
-    python: vars => `${vars['v1']} += ${vars['v2']}`,
-    c: vars => `${vars['v1']} = ${vars['v1']} + ${vars['v2']}`,
-    mips: vars => `LA $t0, ${vars['v2']}\nLW $t1, ($t0)\nLA $t0, ${vars['v1']}\nLW $t2, ($t0)\nADD $t1, $t1, $t2\nSW $t1, ($t0)`,
+    python: vars => [`${vars['v1']} += ${vars['v2']}`],
+    c: vars => [`${vars['v1']} = ${vars['v1']} + ${vars['v2']};`],
+    mips: vars => [
+    `LA $t0, ${vars['v2']}`,
+    `LW $t1, ($t0)`,
+    `LA $t0, ${vars['v1']}`,
+    `LW $t2, ($t0)`,
+    `ADD $t1, $t1, $t2`,
+    `SW $t1, ($t0)`,
+    ],
     initialVars: {
       v1: 'x',
       v2: 'y'
@@ -27,9 +34,15 @@ export default [
     render: (vars, setVars) => <>
       Add {getVariableInput(vars, setVars, 'v2', 'number')} to {getVariableInput(vars, setVars, 'v1')}
     </>,
-    python: vars => `${vars['v1']} += ${vars['v2']}`,
-    c: vars => `${vars['v1']} = ${vars['v1']} + ${vars['v2']}`,
-    mips: vars => `LA $t0, ${vars['v1']}\nLW $t1, ($t0)\nLI $t2, ${vars['v2']}\nADD $t1, $t1, $t2\nSW $t1, ($t0)`,
+    python: vars => [`${vars['v1']} += ${vars['v2']}`],
+    c: vars => [`${vars['v1']} = ${vars['v1']} + ${vars['v2']};`],
+    mips: vars => [
+    `LA $t0, ${vars['v1']}`,
+    `LW $t1, ($t0)`,
+    `LI $t2, ${vars['v2']}`,
+    `ADD $t1, $t1, $t2`,
+    `SW $t1, ($t0)`,
+    ],
     initialVars: {
       v1: 'x',
       v2: 0
